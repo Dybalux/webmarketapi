936bcddd2bde45c39eb6fa06fec76ead88d35c16
Lagoria Luciano <luchicapo@gmail.com>
2025-10-26 01:07:40 -0300

    Revert "Script para la creacion de entorno virtual. Y ejecutables para activar venv en linux y windows. Ejemplo de .env"
    This reverts commit 8facde6fcb1026914503dbea07f7a084cdd71299.

--- activarEntornoLin.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "Activando el entorno virtual (venv)..."
-source venv/bin/activate
-echo "Entorno activado. Escribe 'deactivate' para salir."
-
-#USO
-# chmod +x activarEntornoLin.sh
-# source activarEntornoLin.sh
--- activarEntornoWin.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo Activando el entorno virtual (venv)...
-call ".\venv\Scripts\activate.bat"
-echo Entorno activado. Escribe 'deactivate' para salir.
-
-#.\activar.bat

27382b26fe1312b4a6ef2edf19b16445940166c1
Lagoria Luciano <luchicapo@gmail.com>
2025-10-26 01:20:30 -0300

    Scripts para la creaacion y activacion de venv. Actualizacion de .gitignore

---
+++ activarEntornoLin.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "Activando el entorno virtual (venv)..."
+source venv/bin/activate
+echo "Entorno activado. Escribe 'deactivate' para salir."
+
+#USO
+# chmod +x activarEntornoLin.sh
+# source activarEntornoLin.sh
